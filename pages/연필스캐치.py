eb276e76fcc0722ba95847a0c192e20bd5919083
zeijh01 <119649879+zeijh01@users.noreply.github.com>
2024-08-12 15:43:53 +0900

    Update 연필스캐치.py

--- pages/연필스캐치.py
+++ pages/연필스캐치.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import numpy as np 
 from PIL import Image
-import cv2 
 
-   
 st.write("[알고리즘 참고](https://github.com/amrrs/youtube-r-snippets/blob/master/Create_a_Pencil_Sketch_Portrait_with_Python_OpenCV.ipynb)")
